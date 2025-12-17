b46ffb27b5aa1ae8c134abc1330802dd5196af95
root <root@MSI.localdomain>
2025-12-17 07:49:46 +0000

    Add new version  Helm chart

---
+++ helm-pack.sh
@@ -0,0 +1,16 @@
+helm package nginx-app 
+
+mv nginx-app-* docs/ 
+
+helm repo index docs --url https://phaneendrareddyg.github.io/bookinfo 
+
+cat docs/index.yaml 
+
+git add . 
+
+
+git commit -m "Add new version  Helm chart" 
+
+git push 
+
+curl https://phaneendrareddyg.github.io/bookinfo/index.yaml 
